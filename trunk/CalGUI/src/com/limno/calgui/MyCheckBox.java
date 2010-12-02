c548f059cb9025f4d490bb59b9ebaf66e18c01e6
tad.slawecki@gmail.com <tad.slawecki@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2010-12-02 21:19:36 +0000

    Test upload

---
+++ trunk/CalGUI/src/com/limno/calgui/MyCheckBox.java
@@ -0,0 +1,18 @@
+package com.limno.calgui;
+
+import javax.swing.JCheckBox;
+
+import java.awt.event.*;
+import java.awt.*;
+
+public class MyCheckBox extends JCheckBox {
+	public String switchID;
+	public String FN;
+	
+	public MyCheckBox(String switchID, String FN) {
+		switchID = switchID;
+        FN = FN;
+    }
+	
+	
+	}
