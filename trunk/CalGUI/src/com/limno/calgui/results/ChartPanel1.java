0a3e22bc3fbc36c29f7e08b29c2867e31541e2cd
tad.slawecki@gmail.com <tad.slawecki@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2011-05-31 20:36:21 +0000

    Plots secondary exceedances, corrects "save" label under load scenario.

--- trunk/CalGUI/src/com/limno/calgui/results/ChartPanel1.java
+++ trunk/CalGUI/src/com/limno/calgui/results/ChartPanel1.java
@@ -4,7 +4,12 @@ import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Stroke;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.util.Date;
 
 import hec.heclib.util.HecTime;
@@ -26,16 +31,20 @@ import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.Copies;
+import javax.print.attribute.standard.OrientationRequested;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChartPanel1 extends JPanel {
+public class ChartPanel1 extends JPanel implements Printable {
 	/**
 	 * ChartPanel1 - Creates JPanel with a single ChartPanel
 	 */
 	private static final long serialVersionUID = 7398804723681056388L;
 
-	public ChartPanel1(String title, String yLabel, TimeSeriesContainer[] tscs, TimeSeriesContainer[] stscs,
-			boolean isExceed, Date lower, Date upper) {
+	public ChartPanel1(String title, String yLabel, TimeSeriesContainer[] tscs, TimeSeriesContainer[] stscs, boolean isExceed, Date lower, Date upper) {
 
 		super();
 
@@ -46,13 +55,25 @@ public class ChartPanel1 extends JPanel {
 
 		JFreeChart chart;
 		int primaries = 0;
+		String sName = "";
+		if (stscs != null) {
+			String[] sParts = stscs[0].fullName.split("/");
+			if (sParts.length > 3)
+				sName = sParts[2] + "/" + sParts[3];
+			else
+				sName = "Unparsed Secondary";
+		}
+
 		if (isExceed) {
+
+			// Primary datasets
+
 			XYSeriesCollection dataset = new XYSeriesCollection();
 			XYSeries[] series = new XYSeries[tscs.length];
 			for (int i = 0; i < tscs.length; i++) {
 				series[i] = new XYSeries(tscs[i].fileName);
 				for (int j = 0; j < tscs[i].numberValues; j++) {
-					series[i].add((double) (100.0 * j / (tscs[i].numberValues-1)), tscs[i].values[j]);
+					series[i].add((double) (100.0 * j / (tscs[i].numberValues - 1)), tscs[i].values[j]);
 				}
 				dataset.addSeries(series[i]);
 				if (ymin > tscs[i].minimumValue())
@@ -61,25 +82,29 @@ public class ChartPanel1 extends JPanel {
 					ymax = tscs[i].maxmimumValue(); // typo in HEC DSS classes?
 			}
 
+			// Secondary datasets
+
 			if (stscs != null) {
 				XYSeries[] sseries = new XYSeries[stscs.length];
 				for (int i = 0; i < stscs.length; i++) {
-					sseries[i] = new XYSeries("");
-					for (int j = 0; j < stscs[i].numberValues; j++) {
-						series[i].add((double) (100.0 * j / tscs[i].numberValues), tscs[i].values[j]);
+					if (stscs[i].numberValues > 0) {
+						sseries[i] = new XYSeries(i == 0 ? sName : "");
+						for (int j = 0; j < stscs[i].numberValues; j++) {
+							sseries[i].add((double) (100.0 * j / (stscs[i].numberValues - 1)), stscs[i].values[j]);
+						}
+						dataset.addSeries(sseries[i]);
+						if (ymin > stscs[i].minimumValue())
+							ymin = stscs[i].minimumValue();
+						if (ymax < stscs[i].maxmimumValue())
+							ymax = stscs[i].maxmimumValue(); // typo in HEC DSS
+																// classes?
 					}
-					dataset.addSeries(sseries[i]);
-					if (ymin > stscs[i].minimumValue())
-						ymin = stscs[i].minimumValue();
-					if (ymax < stscs[i].maxmimumValue())
-						ymax = stscs[i].maxmimumValue(); // typo in HEC DSS
-															// classes?
 				}
 			}
 
 			chart = ChartFactory.createXYLineChart(title.replace(";", "+"), // title
 					"Percent", // x-axis label
-					yLabel + ((yLabel.endsWith("(TAF)")?"": "(" + tscs[0].units + ")")), // y-axis label
+					yLabel + ((yLabel.endsWith("(TAF)") ? "" : "(" + tscs[0].units + ")")), // y-axis label
 					dataset, // data
 					true); // create and display a frame...
 
@@ -108,15 +133,7 @@ public class ChartPanel1 extends JPanel {
 				TimeSeries[] sseries = new TimeSeries[stscs.length];
 				for (int i = 0; i < stscs.length; i++) {
 					if (stscs[i].numberValues > 0) {
-						String sName = "";
-						if (i == 0) {
-							String[] sParts = tscs[i].fullName.split("/");
-							if (sParts.length > 3)
-								sName = sParts[2] + "/" + sParts[3];
-							else
-								sName = "Unparsed Secondary";
-						}
-						sseries[i] = new TimeSeries(sName);
+						sseries[i] = new TimeSeries(i == 0 ? sName : "");
 
 						for (int j = 0; j < stscs[i].numberValues; j++) {
 							ht.set(stscs[i].times[j]);
@@ -172,11 +189,15 @@ public class ChartPanel1 extends JPanel {
 		axis.setTickMarkInsideLength(axis.getTickMarkOutsideLength());
 		if (isExceed)
 			axis.setRange(0.0, 100.0);
-//		else {
-//			DateAxis dateAxis = (DateAxis) axis;
-//			dateAxis.setRange(upper, lower);
-//		}
+		// else {
+		// DateAxis dateAxis = (DateAxis) axis;
+		// dateAxis.setRange(upper, lower);
+		// }
 
+		if ((ymax - ymin) < 0.01) {
+			ymax += 0.05;
+			ymin -= 0.05;
+		}
 		axis = plot.getRangeAxis();
 		axis.setTickMarkInsideLength(axis.getTickMarkOutsideLength());
 		axis.setRange(new Range(ymin - 0.05 * (ymax - ymin), ymax + 0.05 * (ymax - ymin)));
@@ -192,7 +213,31 @@ public class ChartPanel1 extends JPanel {
 
 	}
 
+	public void createChartPrintJob() {
+
+		PrintRequestAttributeSet set = new HashPrintRequestAttributeSet();
+		set.add(OrientationRequested.PORTRAIT);
+		set.add(new Copies(1));
+
+		PrinterJob job = PrinterJob.getPrinterJob();
+		job.setPrintable(this);
+		if (job.printDialog(set)) {
+			try {
+				job.print(set);
+			} catch (PrinterException e) {
+				JOptionPane.showMessageDialog(this, e);
+			}
+		}
+
+	}
+
 	private void Work(String title, TimeSeriesContainer[] tscs, TimeSeriesContainer[] stscs) {
 
+	}
+
+	@Override
+	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
