21fbe3513c68b0514717f3bf5bbd9e494e1fe75a
tad.slawecki@gmail.com <tad.slawecki@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2011-06-01 20:23:18 +0000

    Adjust GUI.xml for aesthetics, consistent display of controls

--- CalGUI/src/com/limno/calgui/MainMenu.java
+++ CalGUI/src/com/limno/calgui/MainMenu.java
@@ -174,7 +174,8 @@ public class MainMenu implements ActionListener, ItemListener, MouseListener, Ta
 		swix.getTaglib().registerTag("numtextfield", NumericTextField.class);
 		swix.render(new File(System.getProperty("user.dir") + "\\Config\\GUI.xml")).setVisible(true);
 
-		desktopTitle = desktop.getTitle() + ".172";
+		desktopTitle = desktop.getTitle() + ".173";
+		desktop.setResizable(false);
 
 		scenFilename = ((JTextField) swix.find("run_txfScen")).getText();
 		desktop.setTitle(desktopTitle + " - " + scenFilename);
