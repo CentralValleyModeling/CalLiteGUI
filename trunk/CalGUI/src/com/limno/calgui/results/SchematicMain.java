a7deaf7a247cd5e51a06f561a871b293e3ac6f01
tad.slawecki@gmail.com <tad.slawecki@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2011-08-18 23:36:02 +0000

    Draft code for plotting from schematic

--- trunk/CalGUI/src/com/limno/calgui/results/SchematicMain.java
+++ trunk/CalGUI/src/com/limno/calgui/results/SchematicMain.java
@@ -11,6 +11,7 @@ package com.limno.calgui.results;
 import java.awt.geom.AffineTransform;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import org.apache.batik.script.Window;
@@ -26,20 +27,24 @@ import org.w3c.dom.events.Event;
 import org.w3c.dom.events.EventListener;
 import org.w3c.dom.events.EventTarget;
 
+import com.limno.calgui.MainMenu;
+
 public class SchematicMain {
 
 	JSVGCanvas canvas;
 	Document document;
 	Window window;
+	MainMenu mainMenu;
 
-	public void setAffineTransform(double m0,double m1,double m2,double m3,double m4,double m5) {
-	
-		AffineTransform t = new AffineTransform(m0,m1,m2,m3,m4,m5);
+	public void setAffineTransform(double m0, double m1, double m2, double m3, double m4, double m5) {
+
+		AffineTransform t = new AffineTransform(m0, m1, m2, m3, m4, m5);
 		canvas.setRenderingTransform(t);
 	}
-	
-	public SchematicMain(JPanel p, String url) {
 
+	public SchematicMain(JPanel p, String url, final MainMenu mainMenuIn) {
+
+		mainMenu = mainMenuIn;
 		canvas = new JSVGCanvas();
 
 		// Forces the canvas to always be dynamic even if the current
@@ -49,7 +54,7 @@ public class SchematicMain {
 		canvas.setEnablePanInteractor(true);
 		canvas.setEnableZoomInteractor(true);
 		canvas.setURI(url);
-		
+
 		canvas.addSVGLoadEventDispatcherListener(new SVGLoadEventDispatcherAdapter() {
 			public void svgLoadEventDispatchStarted(SVGLoadEventDispatcherEvent e) {
 				// At this time the document is available...
@@ -64,7 +69,7 @@ public class SchematicMain {
 			}
 		});
 
-		p.add("Center",new JSVGScrollPane(canvas));
+		p.add("Center", new JSVGScrollPane(canvas));
 	}
 
 	public void registerListeners() {
@@ -96,38 +101,87 @@ public class SchematicMain {
 		}
 
 		public void run() {
-			
+
 			AffineTransform t = canvas.getViewBoxTransform();
 			double[] m = new double[6];
 			t.getMatrix(m);
-			for (int i = 0; i<6; i++) System.out.println (m[i]);
+			for (int i = 0; i < 6; i++)
+				System.out.println(m[i]);
 			System.out.println(m);
 
 			String label = null;
-			
+
 			System.out.println("Clicked on: " + evt.getTarget());
 			Element el = ((Element) evt.getTarget());
+			System.out.println("---" + el.getTagName() + " " + el.getNodeName() + el.getNodeType() + el.getNodeValue());
+
 			String tag = el.getTagName();
 			evt.stopPropagation();
-			if (tag.startsWith("polygon") || tag.startsWith("line")) {
-				NodeList childNodes = el.getParentNode().getChildNodes();
-				for (int i = 0; i < childNodes.getLength(); i++) {
-					Node item = childNodes.item(i);
-					if (item instanceof Element) {
-						Element ce = (Element) item;
-						if (ce.getTagName().equals("text")) {
-							label = ce.getTextContent();
-						}
+
+			Element pel = el;
+
+			String ptag = pel.getTagName();
+
+			System.out.println("--" + tag + el.getAttribute("id"));
+			while (el.getParentNode() != null && !ptag.equals("g")) {
+
+				pel = (Element) pel.getParentNode();
+				ptag = pel.getTagName();
+			}
+			System.out.println("Parent: " + ptag);
+
+			if (tag.startsWith("tspan")) // Not sure why, but seems to work
+				pel = (Element) pel.getParentNode();
+
+			NodeList childNodes2 = pel.getChildNodes();
+			for (int i = 0; i < childNodes2.getLength(); i++) {
+				Node item = childNodes2.item(i);
+				if (item instanceof Element) {
+					Element ce = (Element) item;
+					System.out.println("ce = " + ce.getTagName());
+					if (ce.getTagName().equals("title")) {
+						label = ce.getTextContent();
 					}
 				}
-			} else if (tag.startsWith("text")) {
-				label = el.getTextContent();
-			} else if (tag.startsWith("tspan")) {
-				label = el.getParentNode().getTextContent();
 			}
-			if (label != null) {
-				window.alert(label);
+
+			if (label == null) {
+				window.alert("No group identified for this tag");
+			} else {
+				String titleParts[] = label.split("/");
+				if (titleParts.length < 2)
+					window.alert("No link found in group title '" + label + "'.");
+				else {
+					System.out.println(mainMenu.QuickState() + ";Locs-" + titleParts[0] + ";Index-" + titleParts[1]);
+					if (mainMenu.lstScenarios.getModel().getSize() == 0)
+						JOptionPane.showMessageDialog(null, "No scenarios loaded", "Error", JOptionPane.ERROR_MESSAGE);
+					else
+						mainMenu.DisplayFrame(mainMenu.QuickState() + ";Locs-" + titleParts[0] + ";Index-" + titleParts[1]);
+				}
+
 			}
+
+			System.out.println("Title: " + label);
+
+			// if (tag.startsWith("polygon") || tag.startsWith("line")) {
+			// NodeList childNodes = el.getParentNode().getChildNodes();
+			// for (int i = 0; i < childNodes.getLength(); i++) {
+			// Node item = childNodes.item(i);
+			// if (item instanceof Element) {
+			// Element ce = (Element) item;
+			// if (ce.getTagName().equals("text")) {
+			// label = ce.getTextContent();
+			// }
+			// }
+			// }
+			// } else if (tag.startsWith("text")) {
+			// label = el.getTextContent();
+			// } else if (tag.startsWith("tspan")) {
+			// label = el.getParentNode().getTextContent();
+			// }
+			// if (label != null) {
+			// window.alert(tag + ":" + label);
+			// }
 		}
 	}
 
