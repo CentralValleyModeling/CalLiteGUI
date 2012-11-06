cd2f82e510a1eaf67c540e83fe48e282696fcd98
dkrucins@gmail.com <dkrucins@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2012-11-06 20:38:59 +0000

    re-organizing main menu

---
+++ lti_branch/src/com/limno/calgui/GUILinks.java
@@ -0,0 +1,135 @@
+/*
+ * GUILinks: creates a dictionary that relates GUI elements to switchtable order and daat table file/DSS location
+ * 
+ * Version 1.0
+ * 
+ * 2010-12-02
+ *  
+ */
+
+package com.limno.calgui;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+public class GUILinks {
+
+	private Map<String,String> mapCtrlToTable;
+	private Map<String,String> mapCtrlToTID;
+	private Map<String,String> mapCtrlToswitchID;
+	private Map<String,String> mapTIDToCtrl;
+
+	/* 
+	 * GUILinks.tableNameForCtrl: Method to look up table name to use for a given control
+	 * 
+	 * 12/3/2010
+	 * 
+	 */
+	
+	public String tableNameForCtrl(String ctrlID) {
+		return mapCtrlToTable.get(ctrlID);
+	}
+		
+	public String tableIDForCtrl(String ctrlID) {
+		return mapCtrlToTID.get(ctrlID);
+	}
+	
+	public String switchIDForCtrl(String ctrlID) {
+		return mapCtrlToswitchID.get(ctrlID);
+	}
+	
+	public String CtrlFortableID(String tID) {
+		return mapTIDToCtrl.get(tID);
+	}
+	
+	
+	/* 
+	 * GUILinks.readIn: Method to look read control-to-switch, control-to-table information
+	 * 
+	 * 12/3/2010
+	 * 
+	 */
+	
+	public int readIn (String inputFileName) {
+
+		// Open input file
+
+		Scanner input;
+		try 
+		{
+			input = new Scanner(new FileReader(inputFileName));
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Cannot open input file " + inputFileName);
+			return -1;
+		}
+
+		mapCtrlToTable = new HashMap<String,String>();
+		mapCtrlToTID = new HashMap<String,String>();
+		mapCtrlToswitchID = new HashMap<String,String>();
+		mapTIDToCtrl = new HashMap<String,String>();
+		
+		int lineCount = 0;
+		while (input.hasNextLine()) {
+			String line = input.nextLine();
+			lineCount++;
+			if (lineCount > 1) {
+				// Parse, assuming space and/or tab-delimited
+				String[] parts = line.split("[\t]+");
+				if (parts.length > 0) {
+
+					String ctrlName = parts[0];
+					String switchID  = "";
+					String TableID  = "";
+					String tableName = "";
+					if (parts.length > 6) {
+						tableName = parts[6];
+						if (parts.length > 7) {
+							switchID = parts[7];
+						}
+						if (parts.length > 8) {
+							TableID = parts[8];
+						}
+					}
+					mapCtrlToTable.put(ctrlName,tableName);
+					if (switchID != "" ) {
+						mapCtrlToswitchID.put(ctrlName,switchID);
+					}
+					if (TableID != "" ) {
+						mapCtrlToTID.put(ctrlName,TableID);
+						mapTIDToCtrl.put(TableID, ctrlName);
+					}
+				}
+			}
+		}
+
+		input.close();
+
+/*		Set<Map.Entry<String,String>> s = mapCtrlToTable.entrySet();
+
+		//Move next key and value of Map by iterator
+		Iterator<Map.Entry<String,String>> it=s.iterator();
+
+		while(it.hasNext())
+		{
+			// key=value separator this by Map.Entry to get key and value
+			Map.Entry<String,String> m =(Map.Entry<String,String>)it.next();
+
+			// getKey is used to get key of Map
+			String key = (String) m.getKey();
+
+				System.out.println("Key :"+key+"  Value : " + mapCtrlToTable.get(key));
+		}
+
+		for (integer i = 1; i < 15; i++) {
+			System.out.println("Switch: " + Integer.toString(i) + " - " + mapSwitchToCtrl.get(Integer.toString(i)));
+		}
+*/
+		return 0;
+	}
+
+
+}
---
+++ lti_branch/src/com/limno/calgui/ProgressFrame.java
@@ -0,0 +1,52 @@
+package com.limno.calgui;
+
+/*
+ * ProgressFrame - simple frame to show progress passed by method setText();
+ */
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ProgressFrame extends JFrame {
+	private JLabel label;
+
+	public ProgressFrame(String title) {
+
+		super();
+
+		//setUndecorated(true);
+		//getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+		setPreferredSize(new Dimension(400, 100));
+		setMinimumSize(new Dimension(400, 100));
+		setLayout(new BorderLayout());
+
+		setTitle(title);
+		label = new JLabel("",SwingConstants.CENTER);
+		add(label);
+		
+		pack();
+		setAlwaysOnTop(true);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((dim.width - 400) / 2, (dim.height - 100) / 2);
+		java.net.URL imgURL = getClass().getResource("/images/CalLiteIcon.png");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(imgURL));
+		setVisible(true);
+
+		try {
+			Robot robot = new Robot();
+			robot.mouseMove(dim.width / 2, dim.height / 2 + 40);
+		} catch (AWTException e) {
+
+		}
+
+		// TODO - Add close/cancel button
+
+	}
+
+	public void setText(String text) {
+		label.setText(text);
+		repaint();
+	}
+}
