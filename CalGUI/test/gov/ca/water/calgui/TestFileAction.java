9bd8f2e2f48b37fa07f250390bc2e280e4a43f5e
psandhu@water.ca.gov <psandhu@water.ca.gov@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2012-11-26 19:09:05 +0000

    first attempt at writing a test for run action. I expect this will fail and hope to use this as the starting point for refactoring code. Once written correctly will also serve as documentation for code.

---
+++ CalGUI/test/gov/ca/water/calgui/TestFileAction.java
@@ -0,0 +1,28 @@
+package gov.ca.water.calgui;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+
+import junit.framework.TestCase;
+
+import org.swixml.SwingEngine;
+
+public class TestFileAction extends TestCase {
+
+	public void testActionRun() {
+		// everything compiles but data is not set correctly so I expect this to fail
+		JFrame desktop = new JFrame("TestFileAction");
+		SwingEngine swix = new SwingEngine();
+		Boolean[] regUserEdits = new Boolean[] { Boolean.TRUE };
+		String dataFilePath = "\\dataFileDir";
+		int tID = 77777;
+		DataFileTableModel[] dataTableModels = new DataFileTableModel[] { new DataFileTableModel(dataFilePath, tID) };
+		GUILinks guiLinks = new GUILinks();
+		int actionWSIDI = 0;
+		FileAction action = new FileAction(desktop, swix, regUserEdits, dataTableModels, guiLinks, actionWSIDI);
+		int id = 99999;
+		action.actionPerformed(new ActionEvent(desktop, id, "AC_RUN"));
+		assertTrue(true); // test passes if we get to here without an exception
+	}
+}
