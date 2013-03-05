a434f8d920568f9dc2e22ff1f78f814fbda82b40
tad.slawecki@gmail.com <tad.slawecki@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-03-05 20:04:49 +0000

    Add normal/mass-balance radiobuttons to Map View. Basic functionality is there, see Issue 110 for what's not working yet.

---
+++ CalGUI/src/gov/ca/water/calgui/dashboards/SchematicAction.java
@@ -0,0 +1,22 @@
+package gov.ca.water.calgui.dashboards;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.swixml.SwingEngine;
+
+public class SchematicAction implements ActionListener {
+	private final SwingEngine swix;
+
+	public SchematicAction(SwingEngine swix) {
+		this.swix = swix;
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
