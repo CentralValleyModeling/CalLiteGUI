b2b1476e4d572ed665dff1c574a139f85d35a82c
dkrucins@gmail.com <dkrucins@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-05-15 21:00:08 +0000

    Last set of D1485 fixes

--- trunk/CalGUI/src/gov/ca/water/calgui/dashboards/RegListener.java
+++ trunk/CalGUI/src/gov/ca/water/calgui/dashboards/RegListener.java
@@ -32,14 +32,16 @@ public class RegListener implements ItemListener {
 	private final GUILinks gl;
 	private final ButtonGroup reg_btng1;
 	private static Logger log = Logger.getLogger(RegListener.class.getName());
+	private int[] RegFlags;
 
 	public RegListener(SwingEngine swix, Boolean[] RegUserEdits, DataFileTableModel[] dTableModels, GUILinks gl,
-	        ButtonGroup reg_btng1) {
+	        ButtonGroup reg_btng1, int[] RegFlags) {
 		this.swix = swix;
 		this.RegUserEdits = RegUserEdits;
 		this.dTableModels = dTableModels;
 		this.gl = gl;
 		this.reg_btng1 = reg_btng1;
+		this.RegFlags = RegFlags;
 
 	}
 
@@ -65,10 +67,18 @@ public class RegListener implements ItemListener {
 					GUIUtils.toggleEnComponentAndChildren(swix.find("regpan1"), enabled);
 					GUIUtils.toggleEnComponentAndChildren(swix.find("regpan2"), enabled);
 					GUIUtils.toggleEnComponentAndChildren(swix.find("regpan3"), enabled);
-					((JButton) swix.find("btnReg1641")).setEnabled(enabled);
-					((JButton) swix.find("btnReg1485")).setEnabled(enabled);
-					((JButton) swix.find("btnRegCopy")).setEnabled(enabled);
-					((JButton) swix.find("btnRegPaste")).setEnabled(enabled);
+					((JRadioButton) swix.find("btnReg1641")).setEnabled(enabled);
+					((JRadioButton) swix.find("btnReg1485")).setEnabled(enabled);
+					((JRadioButton) swix.find("btnRegUD")).setEnabled(enabled);
+
+					JButton btn = (JButton) swix.find("btnRegCopy");
+					btn.setEnabled(enabled);
+					btn = (JButton) swix.find("btnRegPaste");
+					btn.setEnabled(enabled);
+
+					if (RegFlags == null) {
+						RegFlags = new int[40];
+					}
 
 					if (!cName.startsWith("rdbRegQS_UD")) {
 
@@ -101,10 +111,10 @@ public class RegListener implements ItemListener {
 						input.close();
 
 						String lookups[][];
-						lookups = new String[lineCount][5];
+						lookups = new String[lineCount][6];
 						for (int i = 0; i < lineCount; i++) {
 							String[] parts = allLookups.get(i).split("[\t]+");
-							for (int j = 0; j < 5; j++) {
+							for (int j = 0; j < 6; j++) {
 								if (parts[j].equals("null"))
 									parts[j] = "";
 								lookups[i][j] = parts[j];
@@ -113,22 +123,96 @@ public class RegListener implements ItemListener {
 
 						String ckbName;
 						Boolean b;
+						int rID;
 						for (int i = 0; i < lineCount; i++) {
 							ckbName = lookups[i][0];
 							JCheckBox ckb = (JCheckBox) swix.find(ckbName);
 							b = Boolean.valueOf(lookups[i][iSel + 1]);
 							ckb.setSelected(b);
+							rID = Integer.parseInt(gl.RIDForCtrl(ckbName));
+
+							if (iSel == 1) {
+								RegFlags[rID] = 1;
+							} else if (iSel == 2) {
+								RegFlags[rID] = 1;
+							} else {
+								RegFlags[rID] = 3;
+							}
+
 						}
+					} else {
+
+						Scanner input = null;
+						try {
+							input = new Scanner(new FileReader("Config\\GUI_LinksReg.table"));
+						} catch (FileNotFoundException e) {
+							System.out.println("Cannot open input file Config\\GUI_LinksReg.table");
+						}
+
+						Vector<String> allLookups = new Vector<String>();
+
+						int lineCount = 0;
+						input.nextLine(); // Skip header line
+						while (input.hasNextLine()) {
+							String line = input.nextLine();
+							allLookups.add(line);
+							lineCount++;
+						}
+						input.close();
+
+						String lookups[][];
+						lookups = new String[lineCount][6];
+						for (int i = 0; i < lineCount; i++) {
+							String[] parts = allLookups.get(i).split("[\t]+");
+							for (int j = 0; j < 6; j++) {
+								if (parts[j].equals("null"))
+									parts[j] = "";
+								lookups[i][j] = parts[j];
+							}
+						}
+
+						String ckbName;
+						String NAFlag;
+						int rID;
+						for (int i = 0; i < lineCount; i++) {
+							ckbName = lookups[i][0];
+							NAFlag = lookups[i][5];
+							rID = Integer.parseInt(gl.RIDForCtrl(ckbName));
+							if (NAFlag.equals("FALSE") || ckbName.equals("ckbReg_TRNTY") || ckbName.equals("ckbReg_PUMP")) {
+
+							} else {
+								RegFlags[rID] = 2;
+							}
+						}
+
 					}
 
 				}
+				JCheckBox jbtn = (JCheckBox) swix.find("ckbReg_TRNTY");
+				jbtn.setEnabled(false);
+				jbtn = (JCheckBox) swix.find("ckbReg_PUMP");
+				jbtn.setEnabled(false);
 
 			} else if (cName.startsWith("ckbReg")) {
 				// CheckBox in Regulations panel changed
 				Boolean isSelect = ie.getStateChange() == ItemEvent.SELECTED;
-				RegulationSetup.SetRegCheckBoxes(swix, RegUserEdits, dTableModels, gl, reg_btng1, cName, isSelect, "null");
 
-			} else if (cName.startsWith("reg_rdbUD")) {
+				// if (cName.equals("ckbReg_TRNTY")) {
+				// } else if (cName.equals("ckbReg_PUMP")) {
+				// } else {
+				RegulationSetup
+				        .SetRegCheckBoxes(swix, RegUserEdits, dTableModels, gl, reg_btng1, cName, isSelect, "null", RegFlags);
+				// }
+
+				/*
+				 * JRadioButton btn = (JRadioButton) swix.find("rdbRegQS_UD"); boolean enabled = btn.isEnabled(); ((JRadioButton)
+				 * swix.find("btnReg1641")).setEnabled(enabled); ((JRadioButton) swix.find("btnReg1485")).setEnabled(enabled);
+				 * ((JRadioButton) swix.find("btnRegUD")).setEnabled(enabled); ((JRadioButton)
+				 * swix.find("btnReg1641")).setVisible(enabled); ((JRadioButton) swix.find("btnReg1485")).setVisible(enabled);
+				 * //((JRadioButton) swix.find("btnRegUD")).setVisible(enabled);
+				 */
+
+			} else if (cName.startsWith("btnRegUD")) {
 				// do not allow user edits to tables
 				JTable table = (JTable) swix.find("tblRegValues");
 
@@ -152,8 +236,9 @@ public class RegListener implements ItemListener {
 						ckbtext = ckbtext1[0];
 						ckb.setText(ckbtext + " - User Def.");
 
-						((JButton) swix.find("btnReg1641")).setEnabled(true);
-						((JButton) swix.find("btnReg1485")).setEnabled(true);
+						((JRadioButton) swix.find("btnReg1641")).setEnabled(true);
+						((JRadioButton) swix.find("btnReg1485")).setEnabled(true);
+						((JRadioButton) swix.find("btnRegUD")).setEnabled(true);
 
 					} else {
 
