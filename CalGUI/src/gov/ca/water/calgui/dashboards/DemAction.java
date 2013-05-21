b0221bc65c7c4aa6298e446ec543e4763b94512d
fitzmauricemk <fitzmauricemk@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-05-21 21:01:02 +0000

    Added ActionListner for Demands Screen. Refactored DemAction class to call CalLite.showHelp(String label)

--- CalGUI/src/gov/ca/water/calgui/dashboards/DemAction.java
+++ CalGUI/src/gov/ca/water/calgui/dashboards/DemAction.java
@@ -1,54 +1,31 @@
 package gov.ca.water.calgui.dashboards;
 
-import gov.ca.water.calgui.FileAction;
+import gov.ca.water.calgui.CalLiteHelp;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.net.URL;
 
-import javax.help.HelpSet;
-import javax.help.JHelp;
-import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
-import org.apache.log4j.Logger;
 import org.swixml.SwingEngine;
 
 public class DemAction implements ActionListener {
-	private final SwingEngine swix;
-	static Logger log = Logger.getLogger(DemAction.class.getName());
+	private final SwingEngine SWIX;
 
 	public DemAction(SwingEngine swix) {
-		this.swix = swix;
+		this.SWIX = swix;
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent ae) {
+	public void actionPerformed(ActionEvent actionEvent) {
 
-		if (ae.getActionCommand().equals("AC_Help")) {
+		if (actionEvent.getActionCommand().equals("AC_Help")) {
 
-			JHelp helpViewer;
-			JFrame help;
-			try {
-				ClassLoader classLoader = FileAction.class.getClassLoader();
-				URL url = HelpSet.findHelpSet(classLoader, "../docs/helpset.hs");
-				helpViewer = new JHelp(new HelpSet(classLoader, url));
+			JTabbedPane jtp = (JTabbedPane) SWIX.find("tabbedPane1");
+			String label = jtp.getTitleAt(jtp.getSelectedIndex());
+			CalLiteHelp calLiteHelp = new CalLiteHelp();
+			calLiteHelp.showHelp(label);
 
-				help = new JFrame("CalLite 2.0 GUI Help");
-				help.getContentPane().add(helpViewer);
-
-				JTabbedPane jtp = (JTabbedPane) swix.find("tabbedPane1");
-				int selIndex = jtp.getSelectedIndex();
-				String label = jtp.getTitleAt(selIndex);
-				helpViewer.setCurrentID(label);
-				help.pack();
-				help.setVisible(true);
-
-			} catch (Exception e) {
-
-				log.debug("helpset not found: " + e.getMessage());
-
-			}
 		}
 	}
 }
