eb978d092dafe681027c54d7a5d2fad85f2c4d12
fitzmauricemk <fitzmauricemk@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-05-22 17:34:55 +0000

    Added action logic to stub.

--- CalGUI/src/gov/ca/water/calgui/dashboards/SchematicAction.java
+++ CalGUI/src/gov/ca/water/calgui/dashboards/SchematicAction.java
@@ -1,21 +1,33 @@
 package gov.ca.water.calgui.dashboards;
 
+import gov.ca.water.calgui.CalLiteHelp;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JTabbedPane;
 
 import org.swixml.SwingEngine;
 
 public class SchematicAction implements ActionListener {
-	private final SwingEngine swix;
+	private final SwingEngine SWIX;
 
 	public SchematicAction(SwingEngine swix) {
-		this.swix = swix;
+		this.SWIX = swix;
 
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent actionEvent) {
+
+		if (actionEvent.getActionCommand().equals("AC_Help")) {
+
+			JTabbedPane jtp = (JTabbedPane) SWIX.find("tabbedPane1");
+			String label = jtp.getTitleAt(jtp.getSelectedIndex());
+			CalLiteHelp calLiteHelp = new CalLiteHelp();
+			calLiteHelp.showHelp(label);
+
+		}
 
 	}
 
