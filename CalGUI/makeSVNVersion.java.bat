dd1ffbe9b6b0a86cfaa464be530e221305fe8741
fitzmauricemk <fitzmauricemk@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-06-03 17:29:47 +0000

    Refactored in ant build.

--- CalGUI/makeSVNVersion.java.bat
+++
@@ -1,2 +0,0 @@
-"c:\Program Files (x86)\Subversion\bin\svnversion" -n . > SVNVersion.java.fragment2
-copy /b SVNVersion.java.fragment1+SVNVersion.java.fragment2+SVNVersion.java.fragment3 .\src\gov\ca\water\calgui\utils\SVNVersion.java
