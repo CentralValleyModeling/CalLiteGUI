ab9f35cc466383ec7421033a71d58275a5d4cae3
fitzmauricemk <fitzmauricemk@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-06-04 23:19:31 +0000

    Runs from debugger.

--- trunk/CalGUI/test/gov/ca/water/calgui/TestScenario.java
+++ trunk/CalGUI/test/gov/ca/water/calgui/TestScenario.java
@@ -43,18 +43,32 @@ public class TestScenario {
 
 		FileAction.setupScenario("default.cls", mm.desktop, mm.swix, mm.regUserEditFlags, mm.dTableModels, mm.gl, mm.regFlags);
 
+		// Give Cal-Lite a chance to initialize fully.
+		try {
+
+			System.out.println("Pausing...");
+
+			Thread.sleep(4000);
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		// now check generated files vs baselined generated files for this scenario
 		File directory1 = new File("test-resources/default_scenario/Generated/Lookup");
 
 		// fix this to the actual directory generated
-		File directory2 = new File(System.getProperty("user.dir") + "\\Scenarios\\default\\Generated\\Lookup");
+		File directory2 = new File(System.getProperty("user.dir") + "\\Scenarios\\Run_Details\\default\\Generated\\Lookup");
 		assertDirectoryAllFilesWithSameNameHaveSameContent("Default scenario comparison", directory1, directory2);
+
 	}
 
 	public void assertFileContentIsSame(String msg, File fileExpected, File fileActual) {
 		try {
-			Assert.assertEquals(msg + " " + fileExpected.getAbsolutePath() + " vs " + fileActual.getAbsolutePath(),
-			        FileUtils.contentEquals(fileExpected, fileActual));
+			System.out.println("Testing " + msg);
+
+			Assert.assertEquals(FileUtils.readFileToString(fileExpected).toLowerCase(), FileUtils.readFileToString(fileActual)
+			        .toLowerCase());
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			Assert.fail(msg + ". Failure with exception : " + ex.getMessage());
@@ -71,10 +85,10 @@ public class TestScenario {
 		for (File file1 : files1) {
 			for (File file2 : files2) {
 				if (file1.getName().equals(file2.getName())) {
-					assertFileContentIsSame(msg, file1, file2);
+					assertFileContentIsSame(file1.getName(), file1, file2);
 				}
 			}
-			Assert.fail(msg + "No matching file : " + file1.getName() + " found in " + directory2.getAbsolutePath());
+			// Assert.fail(msg + "No matching file : " + file1.getName() + " found in " + directory2.getAbsolutePath());
 		}
 	}
 }
