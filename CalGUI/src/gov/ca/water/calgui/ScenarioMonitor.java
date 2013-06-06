a2a86036835230709cb596dd7dd62323d3a44320
tad.slawecki@gmail.com <tad.slawecki@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-06-06 18:01:51 +0000

    Updated version of ScenarioMonitor class - no compile error!

--- CalGUI/src/gov/ca/water/calgui/ScenarioMonitor.java
+++ CalGUI/src/gov/ca/water/calgui/ScenarioMonitor.java
@@ -1,12 +1,31 @@
 package gov.ca.water.calgui;
 
+import java.util.ArrayList;
+
+/**
+ * Tracks and displays scenario status during save and run
+ * 
+ * @author tslawecki
+ * 
+ */
 public class ScenarioMonitor {
 
-	private ArrayList<String> scenarioList = new ArrayList<String>();
-	
+	private final ArrayList<String> scenarioList = new ArrayList<String>();
+
+	/**
+	 * Starts monitoring thread
+	 */
 	public void start() {
 	}
 
+	/**
+	 * Adds a scenario to be monitored
+	 * 
+	 * @param scenarioName
+	 */
 	public void add(String scenarioName) {
+		// TODO: Check for duplicate and handle appropriately
+		// TODO: Logging
+		scenarioList.add(scenarioName);
 	}
 }
