2421b70d4a72deaf61fbcd6f82c9c6a40b308feb
tad.slawecki@gmail.com <tad.slawecki@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-06-06 16:59:21 +0000

    Stubbed out class for monitoring status of scenarios when saving, building runs, and running.

---
+++ trunk/CalGUI/src/gov/ca/water/calgui/ScenarioMonitor.java
@@ -0,0 +1,12 @@
+package gov.ca.water.calgui;
+
+public class ScenarioMonitor {
+
+	private ArrayList<String> scenarioList = new ArrayList<String>();
+	
+	public void start() {
+	}
+
+	public void add(String scenarioName) {
+	}
+}
