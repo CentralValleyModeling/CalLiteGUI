38c274c159f320d422385da7e9a5e9818eb61676
tad.slawecki@gmail.com <tad.slawecki@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-06-10 14:23:23 +0000

    More ScenarioMonitor, including a (very simple) test; TestScenario2.java is a fork of TestScenario.java that I want to keep around for now.

--- trunk/CalGUI/src/gov/ca/water/calgui/ScenarioMonitor.java
+++ trunk/CalGUI/src/gov/ca/water/calgui/ScenarioMonitor.java
@@ -1,6 +1,13 @@
 package gov.ca.water.calgui;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import javax.swing.SwingWorker;
+
+import org.apache.log4j.Logger;
 
 /**
  * Tracks and displays scenario status during save and run
@@ -10,12 +17,53 @@ import java.util.ArrayList;
  */
 public class ScenarioMonitor {
 
-	private final ArrayList<String> scenarioList = new ArrayList<String>();
+	private static Properties properties = new Properties();
+	private static Logger log = Logger.getLogger(FileAction.class.getName());
+	private String runRecordFolderName;
+
+	private final HashMap<String, String> scenarioList = new HashMap<String, String>();
+	private final SwingWorker<Void, String> workerScenarioMonitor = new SwingWorker<Void, String>() {
+
+		@Override
+		protected Void doInBackground() throws InterruptedException {
+
+			boolean loop = true;
+			while (loop) {
+				Thread.sleep(100);
+				publish(" ");
+			}
+			return null;
+		}
+
+		@Override
+		protected void process(List<String> stuff) {
+			// TODO: replace console output with Swing GUI
+			System.out.println("ScenarioMonitor: " + scenarioList.size() + " scenarios.");
+			for (int i = 0; i <= scenarioList.size(); i++)
+				System.out.println(scenarioList.get(i));
+
+			return;
+		}
+
+		@Override
+		protected void done() {
+			return;
+
+		}
+
+	};
 
 	/**
 	 * Starts monitoring thread
 	 */
 	public void start() {
+		try {
+			properties.load(FileAction.class.getClassLoader().getResourceAsStream("callite-gui.properties"));
+			runRecordFolderName = properties.getProperty("runrecord.dir", "Run_Records");
+		} catch (Exception e) {
+			log.debug("Problem loading properties. " + e.getMessage());
+		}
+		workerScenarioMonitor.execute();
 	}
 
 	/**
@@ -26,6 +74,33 @@ public class ScenarioMonitor {
 	public void add(String scenarioName) {
 		// TODO: Check for duplicate and handle appropriately
 		// TODO: Logging
-		scenarioList.add(scenarioName);
+		scenarioList.put(scenarioName, "Unknown");
+	}
+
+	private String getStatus(String scenarioName) {
+
+		String scenDir_absPath = new File(System.getProperty("user.dir") + "\\Scenarios\\" + runRecordFolderName + "\\"
+		        + scenarioName).getAbsolutePath();
+
+		File scenDir = new File(scenDir_absPath);
+		if (!scenDir.isDirectory())
+			return "No directory"; // Scenario directory doesn't exist
+
+		File scenSavingFile = new File(scenDir_absPath + "\\saving.txt");
+		if (scenSavingFile.exists())
+			return "Saving"; // Scenario save in progress
+
+		File scenSavedFile = new File(scenDir_absPath + "\\saved.txt");
+		File scenWRESLCHECKFile = new File(scenDir_absPath + "\\WRESLCHECK.txt");
+
+		if (scenSavedFile.exists() && !scenWRESLCHECKFile.exists())
+			return "Saved";
+
+		try
+		
+		File scenPROGRESSFile = new File(scenDir_absPath + "\\PROGRESS.txt");
+		
+
+		return "??";
 	}
 }
