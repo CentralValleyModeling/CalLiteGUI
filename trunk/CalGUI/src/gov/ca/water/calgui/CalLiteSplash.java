b86dda1d4ff571039274e4920d3ea8b55ea613b7
fitzmauricemk <fitzmauricemk@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-06-24 23:28:12 +0000

    Better exception handling.

--- trunk/CalGUI/src/gov/ca/water/calgui/CalLiteSplash.java
+++ trunk/CalGUI/src/gov/ca/water/calgui/CalLiteSplash.java
@@ -10,24 +10,24 @@ import org.apache.log4j.Logger;
 
 public class CalLiteSplash {
 
-	// Place revision/release info on splash screen (to match About screen?)
-
 	private static final SplashScreen splash = SplashScreen.getSplashScreen();
 	private static Properties properties = new Properties();
 	private static Logger log = Logger.getLogger(CalLiteSplash.class.getName());
 
 	public static void getSplash() {
 
+		String buildDate = null;
+		String versionId = null;
+
 		try {
 
 			properties.load(CalLiteSplash.class.getClassLoader().getResourceAsStream("callite-gui.properties"));
+			buildDate = "Build date: " + properties.getProperty("build.date");
+			versionId = "v. " + properties.getProperty("version.id");
 
 		} catch (IOException e) {
-			log.error("Problem in main method:", e);
+			log.error("Problem getting splash screen metadata from properties file: ", e);
 		}
-
-		String buildDate = "Build date: " + properties.getProperty("build.date");
-		String versionId = "v. " + properties.getProperty("version.id");
 
 		Graphics2D g = splash.createGraphics();
 		g.setColor(new Color(255, 255, 204));
