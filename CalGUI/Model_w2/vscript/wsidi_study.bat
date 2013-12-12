f233fff68341756279700c9bec9c050a78a27a73
kkao.office <kkao.office@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-12-12 00:00:56 +0000

    moved vscript's library folders around.

---
+++ CalGUI/Model_w2/vscript/wsidi_study.bat
@@ -0,0 +1,1 @@
+%~dp0..\runConfig_calgui %~dp0..\..\Scenarios\Run_Details\wsidi_example\wsidi_example.config
--- CalGUI/wsidi_generator_test.bat
+++ CalGUI/wsidi_generator_test.bat
@@ -1,2 +1,2 @@
-vscript.bat vscript\Main.py "%~dp0Scenarios\Run_Details\wsidi_example\wsidi_example.config"
+Model_w2\vscript.bat Model_w2\vscript\Main.py "%~dp0Scenarios\Run_Details\wsidi_example\wsidi_example.config"
 pause
