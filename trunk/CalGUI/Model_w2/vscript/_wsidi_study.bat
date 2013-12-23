9aecdf73bc7b3b5c8bb59cf33193fdfdb826f562
tad.slawecki@gmail.com <tad.slawecki@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2013-12-23 15:55:41 +0000

    Remove unused .jar, update wsidi-related files

--- trunk/CalGUI/Model_w2/vscript/_wsidi_study.bat
+++ trunk/CalGUI/Model_w2/vscript/_wsidi_study.bat
@@ -1,1 +1,1 @@
-%~dp0..\runConfig_calgui D:\workspace\CalLiteGUI\Scenarios\Run_Details\wsidi_example\wsidi_example.config
+%~dp0..\runConfig_calgui D:\workspace\CalLiteGUI\Scenarios\Run_Details\DEFAULT\DEFAULT_wsidi.config
