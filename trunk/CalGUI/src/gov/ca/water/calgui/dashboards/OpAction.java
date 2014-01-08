7ada95971d7f10838fb84f4f5179dceb905e85e4
dkrucins@gmail.com <dkrucins@gmail.com@c2e7e3b7-0866-8feb-43e4-40d0a83e0772>
2014-01-08 16:40:08 +0000

    changes and additions to Read, Default, and Edit options on Operations dashboard

--- trunk/CalGUI/src/gov/ca/water/calgui/dashboards/OpAction.java
+++ trunk/CalGUI/src/gov/ca/water/calgui/dashboards/OpAction.java
@@ -8,16 +8,21 @@ import gov.ca.water.calgui.utils.TextTransfer;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.StringTokenizer;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.apache.log4j.Logger;
 import org.swixml.SwingEngine;
@@ -25,7 +30,7 @@ import org.swixml.SwingEngine;
 public class OpAction implements ActionListener {
 	private final SwingEngine swix;
 	private final Boolean[] regUserEdits;
-	private final DataFileTableModel[] dTableModels;
+	private DataFileTableModel[] dTableModels;
 	private final GUILinks gl;
 	private static Logger log = Logger.getLogger(OpAction.class.getName());
 	private final int[] regFlags;
@@ -82,6 +87,114 @@ public class OpAction implements ActionListener {
 			// table.selectAll();
 			table.getActionMap().get(ae1.getActionCommand()).actionPerformed(ae);
 
+		} else if (ae.getActionCommand().startsWith("Op_Read")) {
+			JLabel lab = (JLabel) swix.find("op_WSIDI_Status");
+			String oldText = lab.getText();
+			JPanel pan = (JPanel) swix.find("op_panTab");
+			Boolean cont = true;
+			if (!oldText.contains("Unedited")) {
+				cont = okToContinue(pan);
+			}
+
+			if (cont) {
+				JFileChooser fc = new JFileChooser();
+				fc.setFileFilter(new FileNameExtensionFilter("Data table File *.table", "table"));
+				fc.setCurrentDirectory(new File(".//"));
+
+				// Get SWP File
+				fc.setDialogTitle("Select WSI/DI SWP data table file");
+				int retval = fc.showOpenDialog(fc);
+				if (retval == JFileChooser.APPROVE_OPTION) {
+
+					File fileSWP = fc.getSelectedFile();
+
+					// Get CVP File
+					fc.setDialogTitle("Select WSI/DI CVP data table file");
+					retval = fc.showOpenDialog(fc);
+					if (retval == JFileChooser.APPROVE_OPTION) {
+						File fileCVP = fc.getSelectedFile();
+
+						int tID = Integer.parseInt(gl.tableIDForCtrl("op_btn1"));
+						dTableModels[tID] = new DataFileTableModel(fileSWP.getAbsolutePath(), tID);
+
+						int tID1 = Integer.parseInt(gl.tableIDForCtrl("op_btn2"));
+						dTableModels[tID1] = new DataFileTableModel(fileCVP.getAbsolutePath(), tID1);
+
+						JTable table = (JTable) swix.find("tblOpValues");
+						pan = (JPanel) swix.find("op_panTab");
+						pan.setBorder(BorderFactory.createTitledBorder("SWP"));
+						JComponent component = (JComponent) swix.find("scrOpValues");
+						component.setVisible(true);
+						component.setEnabled(true);
+						table.setVisible(true);
+						JPanel mainmenu = (JPanel) swix.find("mainmenu");
+						mainmenu.revalidate();
+						JComponent component1 = (JComponent) swix.find("scrOpValues");
+
+						dTableModels = PopulateDTable.populate("op_btn2", table, component1, swix, regUserEdits, dTableModels, gl,
+						        regFlags);
+
+						dTableModels = PopulateDTable.populate("op_btn1", table, component1, swix, regUserEdits, dTableModels, gl,
+						        regFlags);
+
+						// Need to add first part of string
+						String[] parts = oldText.split("\\(");
+						lab.setText(parts[0].trim() + " (Unedited)");
+					}
+				}
+
+			}
+
+		} else if (ae.getActionCommand().startsWith("Op_Default")) {
+			JLabel lab = (JLabel) swix.find("op_WSIDI_Status");
+			String oldText = lab.getText();
+			JPanel pan = (JPanel) swix.find("op_panTab");
+			Boolean cont = true;
+			if (!oldText.contains("Unedited")) {
+				cont = okToContinue(pan);
+			}
+
+			if (cont) {
+
+				String comp = "op_btn1";
+				String stID = gl.tableIDForCtrl(comp);
+				int tID = Integer.parseInt(stID);
+
+				DataFileTableModel tm = dTableModels[tID];
+
+				int size = tm.datafiles.length;
+				if (size == 1) {
+					tm.initVectors();
+				} else if (size == 2) {
+					tm.initVectors2();
+				}
+
+				JTable table = (JTable) swix.find("tblOpValues");
+				table.repaint();
+
+				regUserEdits[tID] = false;
+
+				comp = "op_btn2";
+				stID = gl.tableIDForCtrl(comp);
+				tID = Integer.parseInt(stID);
+
+				tm = dTableModels[tID];
+
+				size = tm.datafiles.length;
+				if (size == 1) {
+					tm.initVectors();
+				} else if (size == 2) {
+					tm.initVectors2();
+				}
+
+				table.repaint();
+
+				regUserEdits[tID] = false;
+
+				String[] parts = oldText.split("\\(");
+				lab.setText(parts[0].trim() + " (Unedited)");
+			}
+
 		} else if (ae.getActionCommand().startsWith("Op_Paste")) {
 
 			// System.out.println("Clipboard contains:" +
@@ -127,6 +240,29 @@ public class OpAction implements ActionListener {
 				calLiteHelp.showHelp(label);
 			}
 		}
+	}
+
+	public Boolean okToContinue(JComponent comp) {
+		Boolean result = true;
+
+		int n = JOptionPane.showConfirmDialog(comp,
+		        "WSI/DI data tables have been modified.  Are you sure you wish to overwrite these changes?", "CalLite Gui",
+		        JOptionPane.YES_NO_CANCEL_OPTION);
+
+		switch (n) {
+		case JOptionPane.CANCEL_OPTION:
+			result = false;
+			break;
+
+		case JOptionPane.NO_OPTION:
+			result = false;
+			break;
+
+		case JOptionPane.YES_OPTION:
+			result = true;
+			break;
+		}
+		return result;
 	}
 
 }
